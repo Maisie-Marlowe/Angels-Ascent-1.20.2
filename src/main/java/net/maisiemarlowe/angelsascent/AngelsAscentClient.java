ef5ccb846f18ff75e978497e65fc474e47f89f68
Maisie Marlowe <eclipsemail38@gmail.com>
2024-07-16 23:34:07 -0400

    Initial commit

---
+++ src/main/java/net/maisiemarlowe/angelsascent/AngelsAscentClient.java
@@ -0,0 +1,10 @@
+package net.maisiemarlowe.angelsascent;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AngelsAscentClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/maisiemarlowe/angelsascent/AngelsAscentDataGenerator.java
@@ -0,0 +1,11 @@
+package net.maisiemarlowe.angelsascent;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AngelsAscentDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
