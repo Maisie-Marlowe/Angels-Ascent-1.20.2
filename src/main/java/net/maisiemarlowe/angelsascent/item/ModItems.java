e825879b49db3d06fcaf69a1c95ff3e59c25f83d
Maisie Marlowe <eclipsemail38@gmail.com>
2024-07-30 10:32:33 -0400

    Added Ore Generation (and backups) multiple items and recipes.

--- src/main/java/net/maisiemarlowe/angelsascent/item/ModItems.java
+++ src/main/java/net/maisiemarlowe/angelsascent/item/ModItems.java
@@ -40,7 +40,7 @@ public class ModItems {
     public static final Item HOLY_SHOVEL = registerItem("holy_shovel", new ModShovelItems(ModToolMaterials.HOLY_BAR,
             .5f, -2.6f, true, new FabricItemSettings().fireproof()));
 
-    public static final Item ANGEL_WINGS = registerItem("angel_wings", new WingsItem(new FabricItemSettings()));
+    //public static final Item ANGEL_WINGS = registerItem("angel_wings", new WingsItem(new FabricItemSettings()));
 
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
